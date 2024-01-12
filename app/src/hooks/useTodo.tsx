959fbd1b921d41af01e35e90f413134ffc2e81aa
Oleg Chepelev <yorkblansh@gmail.com>
2024-01-12 07:21:18 +0300

    upd

--- app/src/hooks/useTodo.tsx
+++ app/src/hooks/useTodo.tsx
@@ -1,6 +1,6 @@
-import { createWideHook } from "widehook";
-import { TodoItem } from "../components/todo/Item";
+import { createWideHook } from 'widehook'
+import { TodoItem } from '../components/todo/Item'
 
 export const useToDo = createWideHook({
-  init: [{ id: 0, name: "first", isDone: false }] as TodoItem[],
-});
+	init: [{ id: 0, name: 'first', isDone: false }] as TodoItem[],
+})
